55304af0a9c03b1c321e07b66e485a380683660b
avcshka <forceregeneration@gmail.com>
2022-09-14 11:39:48 +0300

    feat: practice number 2

---
+++ practiceNumber6.js
@@ -0,0 +1,32 @@
+// ДЗ после 27 урока
+function sayHello(name) {
+    return `Hello, ${name}`
+}
+
+console.log(sayHello('Артем'))
+
+function returnNeighboringNumbers(num) {
+    return [num - 1, num, num + 1]
+}
+
+console.log(returnNeighboringNumbers(5));
+
+function getMathResult(num, repeat) {
+    if (typeof(repeat) != 'number' || repeat <= 0) {
+        return num;
+    }
+
+    let result = '';
+
+    for (let i = 1; i <= repeat; i++) {
+        if (i === repeat) {
+            result += `${num * i}`;
+        } else {
+            result += `${num * i}---`;
+        }
+        console.log(result);
+    }
+    return result;
+}
+
+console.log(getMathResult(5, 3));
