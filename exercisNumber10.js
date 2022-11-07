fa490615589e768edd8b49f7e8a899aa7c351c31
avcshka <forceregeneration@gmail.com>
2022-11-07 14:52:26 +0200

    feat: exercise 10 working with objects: const {} = object. Repeat: forEach(callback), for (let key in object) {}

---
+++ exercisNumber10.js
@@ -0,0 +1,40 @@
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function (plan) {
+        const {age} = plan
+        const {languages} = plan.skills
+        let str = `Мне ${age} и я владею языками: `;
+
+        languages.forEach(function (lang) {
+            str += `${lang.toUpperCase()} `;
+        })
+        return str;
+    }
+};
+
+function showExperience(plan) {
+    const {exp} = plan.skills;
+    return exp;
+}
+
+function showProgrammingLangs(plan) {
+    let str = '';
+    const {programmingLangs} = plan.skills
+    for (let key in programmingLangs) {
+        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
+    }
+    return str;
+}
+
+console.log('showExp: ' + showExperience(personalPlanPeter));
+console.log('showProgLangs: ' + showProgrammingLangs(personalPlanPeter));
+console.log('showAgeAndLagns: ' + personalPlanPeter.showAgeAndLangs(personalPlanPeter))
